d84cc6fc91355c074c63d35ecaf8eded550a43c3
Zak Mayfield <zakmayfield@gmail.com>
2023-04-20 14:56:32 -0400

    scraper

---
+++ scraper.py
@@ -0,0 +1,34 @@
+from bs4 import BeautifulSoup
+import requests
+import os
+import urllib.request
+from requests.compat import urljoin
+
+chasers_url = 'https://chasersjuice.com/'
+response = requests.get(chasers_url)
+soup = BeautifulSoup(response.text, 'html.parser')
+img_tags = soup.find_all('img')
+div_tags = soup.find_all('div', {'style': 'background-image: url('})
+urls = [urljoin(chasers_url, img['src']) for img in img_tags]
+urls += [urljoin(chasers_url, div['style'][23:-3]) for div in div_tags]
+
+# no dupes
+urls = list(set(urls))
+
+file = open("links.txt", "w")
+
+for url in urls:
+    try:
+        print(url)
+        file.write(url + '\n')
+    except:
+        print('failed to write url')
+
+file.close()
+
+for i, url in enumerate(urls):
+    try:
+        print(i, url)
+        urllib.request.urlretrieve(url, 'images/image' + str(i) + '.jpg')
+    except Exception as e:
+        print('failed to download image:', e)
